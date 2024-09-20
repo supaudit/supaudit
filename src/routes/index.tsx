1657818d8270ebb8b562aa49108f695f6d4ee43a
Óscar Carrasco <me@oxca.be>
2024-09-20 23:48:09 +0100

    feat: add pandacss, shadow-panda, custom fonts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,27 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { routeLoader$ } from "@builder.io/qwik-city";
+import { createServerClient } from "supabase-auth-helpers-qwik";
+
+import { css } from "~/styled-system/css";
+import { button } from '@shadow-panda/styled-system/recipes'
+
 
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
+      <h1 class={css({fontFamily: "Space Grotesk Variable", fontSize: "6xl", fontWeight: 700})}>Supaudit</h1>
+      <button class={button({ variant: 'outline' })}>Login</button>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Generate security audit reports · Supaudit",
   meta: [
     {
-      name: "description",
-      content: "Qwik site description",
+      name: "Supaudit",
+      content: "Generate security audit reports",
     },
   ],
 };
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,6 +1,9 @@
 import { component$, Slot } from "@builder.io/qwik";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
+import "@fontsource-variable/public-sans";
+import "@fontsource-variable/space-grotesk";
+
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
   // https://qwik.dev/docs/caching/
