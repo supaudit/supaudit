11bb23a7e2709701ac194ef40a9a3349e942645f
Eduardo González <codexlynx@gmail.com>
2024-09-21 14:01:04 +0200

    fix(conflicts): Add resolve conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,6 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
  */
 
 export default defineConfig(({ command, mode }): UserConfig => {
-<<<<<<< Updated upstream
   return {
     plugins: [
       macroPlugin({ preset: "pandacss" }),
@@ -63,47 +62,6 @@ export default defineConfig(({ command, mode }): UserConfig => {
       },
     },
   };
-=======
-	return {
-		plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
-		// This tells Vite which dependencies to pre-build in dev mode.
-		optimizeDeps: {
-			// Put problematic deps that break bundling here, mostly those with binaries.
-			// For example ['better-sqlite3'] if you use that in server functions.
-			exclude: [],
-		},
-
-		/**
-		 * This is an advanced setting. It improves the bundling of your server code. To use it, make sure you understand when your consumed packages are dependencies or dev dependencies. (otherwise things will break in production)
-		 */
-		// ssr:
-		//   command === "build" && mode === "production"
-		//     ? {
-		//         // All dev dependencies should be bundled in the server build
-		//         noExternal: Object.keys(devDependencies),
-		//         // Anything marked as a dependency will not be bundled
-		//         // These should only be production binary deps (including deps of deps), CLI deps, and their module graph
-		//         // If a dep-of-dep needs to be external, add it here
-		//         // For example, if something uses `bcrypt` but you don't have it as a dep, you can write
-		//         // external: [...Object.keys(dependencies), 'bcrypt']
-		//         external: Object.keys(dependencies),
-		//       }
-		//     : undefined,
-
-		server: {
-			headers: {
-				// Don't cache the server response in dev mode
-				"Cache-Control": "public, max-age=0",
-			},
-		},
-		preview: {
-			headers: {
-				// Do cache the server response in preview (non-adapter production build)
-				"Cache-Control": "public, max-age=600",
-			},
-		},
-	};
->>>>>>> Stashed changes
 });
 // *** utils ***
 /**
@@ -115,7 +73,6 @@ function errorOnDuplicatesPkgDeps(
 	devDependencies: PkgDep,
 	dependencies: PkgDep,
 ) {
-<<<<<<< Updated upstream
   let msg = "";
   // Create an array 'duplicateDeps' by filtering devDependencies.
   // If a dependency also exists in dependencies, it is considered a duplicate.
@@ -142,37 +99,4 @@ function errorOnDuplicatesPkgDeps(
   if (duplicateDeps.length > 0) {
     throw new Error(msg);
   }
-=======
-	let msg = "";
-	// Create an array 'duplicateDeps' by filtering devDependencies.
-	// If a dependency also exists in dependencies, it is considered a duplicate.
-	const duplicateDeps = Object.keys(devDependencies).filter(
-		(dep) => dependencies[dep],
-	);
-
-	// include any known qwik packages
-	const qwikPkg = Object.keys(dependencies).filter((value) =>
-		/qwik/i.test(value),
-	);
-
-	// any errors for missing "qwik-city-plan"
-	// [PLUGIN_ERROR]: Invalid module "@qwik-city-plan" is not a valid package
-	msg = `Move qwik packages ${qwikPkg.join(", ")} to devDependencies`;
-
-	if (qwikPkg.length > 0) {
-		throw new Error(msg);
-	}
-
-	// Format the error message with the duplicates list.
-	// The `join` function is used to represent the elements of the 'duplicateDeps' array as a comma-separated string.
-	msg = `
-    Warning: The dependency "${duplicateDeps.join(", ")}" is listed in both "devDependencies" and "dependencies".
-    Please move the duplicated dependencies to "devDependencies" only and remove it from "dependencies"
-  `;
-
-	// Throw an error with the constructed message.
-	if (duplicateDeps.length > 0) {
-		throw new Error(msg);
-	}
->>>>>>> Stashed changes
 }
