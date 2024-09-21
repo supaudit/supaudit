ef3445894152d05716d99773ab653ccaf4013ff2
Óscar Carrasco <me@oxca.be>
2024-09-21 14:45:28 +0100

    feat: add global types

---
+++ types.ts
@@ -0,0 +1,45 @@
+/**
+ * Represents a collection of records.
+ */
+export type Records = Record<string, string[]>;
+
+/**
+ * Represents the information of an HTTP response.
+ */
+export interface HttpHesponseInfo {
+    status: number;
+    headers: Record<string, string>;
+}
+
+/**
+ * Represents information about an HTTP path.
+ */
+export interface HttpPathInfo {
+    url: string;
+    response: HttpHesponseInfo;
+}
+
+/**
+ * Represents information about an HTTP request.
+ */
+export interface HttpInfo {
+    https: boolean;
+    paths: Record<HttpPathInfo["url"], HttpPathInfo>;
+}
+
+/**
+ * Represents a domain.
+ */
+export interface Domain {
+    name: string;
+    records: Records;
+    subdomains: Domain[];
+    http: HttpInfo;
+}
+
+/**
+ * Represents the state of an audit report.
+ */
+export interface AuditReportState {
+    domains: Record<Domain["name"], Domain>;
+}
